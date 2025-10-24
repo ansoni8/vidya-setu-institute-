ad077f8f14c4eb564e54cf4eb89aec1fc63519a3
ansoni8 <148438972+ansoni8@users.noreply.github.com>
2025-10-25 03:25:24 +0530

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export const metadata = {
+  title: "Vidya Setu Institute",
+  description: "Premium learning experience",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="bg-[#F8FAFD] text-[#111827]">
+        <Navbar />
+        <main className="min-h-screen">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ page.tsx
@@ -0,0 +1,13 @@
+import HeroSection from "../components/HeroSection";
+import About from "./about/page";
+import Courses from "./courses/page";
+
+export default function Home() {
+  return (
+  <>
+    <HeroSection />
+    <Courses/>
+    <About />
+  </>
+  );
+}
